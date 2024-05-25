786a108bc3603a99116296f308862ee0fb3b6257
seversoftech <developersever@gmail.com>
2024-05-25 16:08:45 +0100

    all

--- lib/tabs/gpa.dart
+++ lib/tabs/gpa.dart
@@ -241,27 +241,52 @@ class _GpaState extends State<Gpa> with AutomaticKeepAliveClientMixin<Gpa> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.start,
           children: [
-            TextField(
-              controller: fname,
-              decoration: InputDecoration(
-                  border: InputBorder.none,
-                  hintText: 'Full Name',
-                  hintStyle:
-                      TextStyle(fontFamily: headerFont, color: numberColor)),
-              autofocus: true,
-              style: TextStyle(fontFamily: numberFont, color: numberColor),
-              keyboardType: TextInputType.text,
+            Container(
+              margin: const EdgeInsets.only(left: 5, top: 10, right: 5),
+              padding: const EdgeInsets.only(
+                left: 10,
+                right: 5,
+              ),
+              decoration: BoxDecoration(
+                color: Colors.purple[100],
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(5.0),
+                ),
+                border: Border.all(color: listLineColor),
+              ),
+              child: TextField(
+                controller: fname,
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Enter full Name',
+                    hintStyle:
+                        TextStyle(fontFamily: headerFont, color: numberColor)),
+                autofocus: true,
+                style: TextStyle(fontFamily: numberFont, color: numberColor),
+                keyboardType: TextInputType.text,
+              ),
             ),
-            TextField(
-              controller: regno,
-              decoration: InputDecoration(
-                  border: InputBorder.none,
-                  hintText: 'Reg No',
-                  hintStyle:
-                      TextStyle(fontFamily: headerFont, color: numberColor)),
-              autofocus: true,
-              style: TextStyle(fontFamily: numberFont, color: numberColor),
-              keyboardType: TextInputType.text,
+            Container(
+              margin: const EdgeInsets.only(left: 5, top: 10, right: 5),
+              padding: const EdgeInsets.only(left: 10, right: 5),
+              decoration: BoxDecoration(
+                color: Colors.purple[100],
+                borderRadius: const BorderRadius.all(Radius.circular(5.0)),
+                border: Border.all(
+                  color: listLineColor,
+                ),
+              ),
+              child: TextField(
+                controller: regno,
+                decoration: InputDecoration(
+                    border: InputBorder.none,
+                    hintText: 'Enter Matric. No',
+                    hintStyle:
+                        TextStyle(fontFamily: headerFont, color: numberColor)),
+                autofocus: true,
+                style: TextStyle(fontFamily: numberFont, color: numberColor),
+                keyboardType: TextInputType.text,
+              ),
             ),
             _rowForResullt(
                 'Credit Hours   :', calculation.creditHours.toString()),
